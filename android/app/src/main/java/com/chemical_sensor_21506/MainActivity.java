7ada8de78919cb39b183c5699abdb405fc20a958
Crowdbotics <team@crowdbotics.com>
2020-10-14 20:41:16 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/chemical_sensor_21506/MainActivity.java
@@ -0,0 +1,15 @@
+package com.chemical_sensor_21506;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "chemical_sensor_21506";
+    }
+}
